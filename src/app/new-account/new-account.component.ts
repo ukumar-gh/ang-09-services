71e9a551fbe2e8453445e2a32c1df4c65a2cfa88
Kumar MA <public.kumar@gmail.com>
2024-02-12 13:05:00 -0800

    logging service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -6,13 +7,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  
+  constructor(private loggingService: LoggingService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+
   }
 }
