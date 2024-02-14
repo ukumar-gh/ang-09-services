c20fd3723ee6d3d8c9c1c25e3a0bd234457ac14b
Kumar MA <public.kumar@gmail.com>
2024-02-14 09:49:48 -0800

    112e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,20 @@
 import { Component } from '@angular/core';
+import { AccountsService } from './accounts.service';import { AccountComponent } from './account/account.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService, AccountComponent]
 })
 export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+  accounts: { name: string, status: string }[] = [];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+  constructor(private accountsService: AccountsService) { }
+  
+  ngOnInit() {
+    this.accounts = this.accountsService.accounts;
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
 }
