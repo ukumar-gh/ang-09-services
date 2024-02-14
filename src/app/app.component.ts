1653177b1a10e2813863f40bdf30430411c1cad6
Kumar MA <public.kumar@gmail.com>
2024-02-14 09:54:01 -0800

    113e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { AccountsService } from './accounts.service';import { AccountComponent } from './account/account.component';
-
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AccountsService, AccountComponent]
+  providers: [AccountsService]
 })
 export class AppComponent {
   accounts: { name: string, status: string }[] = [];
