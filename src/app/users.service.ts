2714b0b00153403e154ff1579c640211bbe7bb56
Kumar MA <public.kumar@gmail.com>
2024-02-17 11:58:08 -0800

    118e

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -3,10 +3,12 @@ export class userService {
   inactiveUsers = ['Chris', 'Mabu'];
 
   setToActive(id: number) {
-    
+    this.activeUsers.push(this.inactiveUsers[id]);
+    this.inactiveUsers.splice(id, 1);
   }
-
+  
   setToInactive(id: number) {
-    
+    this.inactiveUsers.push(this.activeUsers[id]);
+    this.activeUsers.splice(id, 1);
   }
 }
